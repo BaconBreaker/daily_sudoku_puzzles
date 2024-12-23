a210c59d2e30472a2b62b7478adfef80a000445c
BaconBreaker <thomas.christensen2712@gmail.com>
2024-12-23 11:17:09 +0100

    hard puzzles are actually hard again

--- generate_puzzles.sh
+++ generate_puzzles.sh
@@ -13,7 +13,7 @@ cd sudoku_solver_generator_golang
 echo "Generating puzzles"
 go run . -task "generate" -n_filled_cells 38 -save_path "../puzzles/$DATE/easy.txt"
 go run . -task "generate" -n_filled_cells 30 -save_path "../puzzles/$DATE/medium.txt"
-go run . -task "generate" -n_filled_cells 30 -save_path "../puzzles/$DATE/hard.txt"
+go run . -task "generate" -n_filled_cells 25 -save_path "../puzzles/$DATE/hard.txt"
 
 #find solutions
 echo "Finding solutions"
