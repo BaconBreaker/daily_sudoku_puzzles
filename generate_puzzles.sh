bfa9df205bb23c7fbadf60f634cddb0fa3a3caa5
BaconBreaker <thomas.christensen2712@gmail.com>
2024-12-23 11:10:52 +0100

    Now also updates README.md

--- generate_puzzles.sh
+++ generate_puzzles.sh
@@ -1,6 +1,8 @@
 DATE="$(date +%Y-%m-%d)"
+DATE_YESTERDAY="$(date -d 'yesterday' +%Y-%m-%d)"
 
 echo $DATE
+echo $DATE_YESTERDAY
 
 git clone https://github.com/BaconBreaker/sudoku_solver_generator_golang.git
 mkdir puzzles/$DATE/
@@ -11,7 +13,7 @@ cd sudoku_solver_generator_golang
 echo "Generating puzzles"
 go run . -task "generate" -n_filled_cells 38 -save_path "../puzzles/$DATE/easy.txt"
 go run . -task "generate" -n_filled_cells 30 -save_path "../puzzles/$DATE/medium.txt"
-go run . -task "generate" -n_filled_cells 25 -save_path "../puzzles/$DATE/hard.txt"
+go run . -task "generate" -n_filled_cells 30 -save_path "../puzzles/$DATE/hard.txt"
 
 #find solutions
 echo "Finding solutions"
@@ -20,3 +22,39 @@ go run . -task "solve" -file_path "../puzzles/$DATE/medium.txt" -save_path "../s
 go run . -task "solve" -file_path "../puzzles/$DATE/hard.txt" -save_path "../solutions/$DATE/hard.txt"
 
 cd ..
+
+print_func () {
+    echo "\`\`\`" >> "$2"
+    while IFS= read -r line
+    do
+        echo "$line" >> "$2"
+    done < "$1"
+    echo "\`\`\`" >> "$2"
+}
+
+echo "" > README.md
+
+echo "# daily_sudoku_puzzles \n" >> README.md
+
+echo "Collection of daily sudoku puzzles automated with GitHub workflows \n" >> README.md
+echo "Puzzles are generated using my other library https://github.com/BaconBreaker/sudoku_solver_generator_golang \n \n" >> README.md
+
+echo "# Todays puzzles are \n" >> README.md
+echo "## Easy \n" >> README.md
+print_func "puzzles/$DATE/easy.txt" "README.md"
+
+echo "## Medium \n" >> README.md
+print_func "puzzles/$DATE/medium.txt" "README.md"
+
+echo "## Hard \n" >> README.md
+print_func "puzzles/$DATE/hard.txt" "README.md"
+
+echo "# Yesterdays solutions are \n" >> README.md
+echo "## Easy \n" >> README.md
+print_func "solutions/$DATE_YESTERDAY/easy.txt" "README.md"
+
+echo "## Medium \n" >> README.md
+print_func "solutions/$DATE_YESTERDAY/medium.txt" "README.md"
+
+echo "## Hard \n" >> README.md
+print_func "solutions/$DATE_YESTERDAY/hard.txt" "README.md"
